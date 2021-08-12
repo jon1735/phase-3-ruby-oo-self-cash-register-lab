138123ee001d2c87918c0951232d249daca4f215
Jonathan Gonzalez <Jonathan.gonzalez@powerhrg.com>
2021-08-12 08:23:48 -0400

    commit tree 50c813ce3100f446add5f9a592ccb96b5ffacf80

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,11 @@
+require "pry"
 
+class CashRegister
+  attr_accessor :items, :discount, :total
+
+  def initialization(discount = 0)
+    @items = []
+    @discount = discount
+    @total = 0
+  end
+end
